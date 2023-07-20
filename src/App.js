3bfbae6394f2d733832cd6d27814b3b1ca87f9bf
Ariel Stitt <astitt@perscholas.org>
2023-07-20 10:00:59 -0400

    looped through an object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Arrays from './Arrays'
+import Objects from './Objects';
 
 
 function App() {
   return (
     <div className="App">
      <h1>React Drills</h1>
-     <Arrays />
+     {/* <Arrays /> */}
+     <Objects />
     </div>
   );
 }
