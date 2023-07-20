97d3c0e8fbeecf2662aa844846b4bd8bb50b6620
Ariel Stitt <astitt@perscholas.org>
2023-07-20 10:52:54 -0400

    created counter using state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import Arrays from './Arrays'
 import Objects from './Objects';
+import {useState } from 'react'
 
 
 function App() {
+  
+
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1)
+  }
+  const handleDecrement = () => {
+    
+  }
+
   return (
     <div className="App">
      <h1>React Drills</h1>
      {/* <Arrays /> */}
-     <Objects />
+     {/* <Objects /> */}
+    <h1> {counter} </h1>
+     <button onClick={handleIncrement}> + </button>
+     <button onClick={()=> {setCounter(counter - 1)}}> - </button>
+      <br />
+      <button onClick={()=> {setCounter(0)}}>reset</button>
     </div>
   );
 }
--- src/Objects.jsx
+++ src/Objects.jsx
@@ -8,6 +8,13 @@ function Objects() {
         email: 'abc@123.com'
     }
 
+    const {firstName, age, email} = zuko;
+    /*
+    const firstNmae = zuko.firstName;
+    const age = zuko.age;
+    const email = zuko.email;
+    */
+
     return Object.keys(zuko).map((key, index)=>{
         return (
             <h2>
